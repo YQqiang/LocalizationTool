145c4ec0028e801809d60e76b08b6ad5a14a4603
yuqiang <1054572107@qq.com>
2017-12-13 10:39:43 +0800

    Initial Commit

---
+++ LocalizationTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LocalizationTool
+//
+//  Created by sungrow on 2017/12/13.
+//  Copyright © 2017年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LocalizationTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LocalizationTool
+//
+//  Created by sungrow on 2017/12/13.
+//  Copyright © 2017年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
